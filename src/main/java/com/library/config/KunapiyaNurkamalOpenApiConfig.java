3d1745505a0b381034316f5cc007fc8892979050
nurkamalkunapia-web <nurkamalkunapia@gmail.com>
2026-05-21 17:20:18 +0500

    Added a custom OpenAPI configuration bean with a security scheme mapping for JWT Bearer Authentication.

--- src/main/java/com/library/config/KunapiyaNurkamalOpenApiConfig.java
+++ src/main/java/com/library/config/KunapiyaNurkamalOpenApiConfig.java
@@ -1,24 +1,27 @@
 package com.library.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Library Management API",
-                version = "1.0",
-                description = "Backend API for Library Management System"
-        )
-)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
 public class KunapiyaNurkamalOpenApiConfig {
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+                final String securitySchemeName = "bearerAuth";
+                return new OpenAPI()
+                        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                        .components(new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                ));
+        }
 }
