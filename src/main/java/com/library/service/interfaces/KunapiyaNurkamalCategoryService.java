7fb1ce7098eea69f83ac839df7e04ba6dd49981b
nurkamalkunapia-web <nurkamalkunapia@gmail.com>
2026-05-22 05:14:53 +0500

    service upd

--- src/main/java/com/library/service/interfaces/KunapiyaNurkamalCategoryService.java
+++ src/main/java/com/library/service/interfaces/KunapiyaNurkamalCategoryService.java
@@ -6,13 +6,23 @@ import com.library.dto.response.KunapiyaNurkamalCategoryResponse;
 import java.util.List;
 
 public interface KunapiyaNurkamalCategoryService {
-    KunapiyaNurkamalCategoryResponse createCategory(KunapiyaNurkamalCategoryRequest request);
 
-    KunapiyaNurkamalCategoryResponse getCategoryById(Long id);
+    KunapiyaNurkamalCategoryResponse createCategory(
+            KunapiyaNurkamalCategoryRequest request
+    );
+
+    KunapiyaNurkamalCategoryResponse getCategoryById(
+            Long id
+    );
 
     List<KunapiyaNurkamalCategoryResponse> getAllCategories();
 
-    KunapiyaNurkamalCategoryResponse updateCategory(Long id, KunapiyaNurkamalCategoryRequest request);
+    KunapiyaNurkamalCategoryResponse updateCategory(
+            Long id,
+            KunapiyaNurkamalCategoryRequest request
+    );
 
-    void deleteCategory(Long id);
+    void deleteCategory(
+            Long id
+    );
 }
